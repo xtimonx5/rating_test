471c26c84e730dd55d727dd56ab0f964d2a3db09
Dmitry Gavrilenko <d.gavrilenko@invento.by>
2019-04-18 11:38:09 +0300

    added api tests

---
+++ README.md
@@ -0,0 +1,31 @@
+Application is accepting amqp messages in `rating_message_queue`  queue. 
+(Can be changed in settings), building PostgreSQL materialized view with rate place. 
+Materialized view refreshes every minute.
+
+
+
+To run application:
+1. clone this repo
+2. make `docker-compose build`
+3. make `docker-compose up` (default env variables will run django runserver)
+
+
+Allowed endpoints:
+
+1. `127.0.0.1:8000/api/leaderboard/` - paginated leaderboard.
+
+    allowed query_params: 
+        1.1`?rate_place=N`, - concrete rate place (N) 
+        
+        1.2`?rate_place__gt=N` - rate place greater than N
+        
+        1.3`?rate_place__gte=N` - rate place greater or equal to N
+        
+        1.4.`?rate_place__lt=N` - rate place lower than N
+        
+        1.5.`?rate_place__lte=N` - rate place lower or equal to N
+        
+        1.6.`?user_id=N` - user id is N
+ 
+ 
+2. `127.0.0.1/api/leaderboard/${N}` - To find who is on N place and also it's "neighbors" 
--- rating/tests/test_api.py
+++ rating/tests/test_api.py
@@ -35,3 +35,26 @@ class LeaderBoardApiTestCase(CommonRatingTest):
         response_dict = json.loads(response.content)
         self.assertEqual(response_dict['count'], 100)
         self.assertEqual(len(response_dict['results']), 5)
+
+    def test_query_params(self):
+        # testing exact rate place
+        response_exact_rate_place = self.client.get('/api/leaderboard/?rate_place=5')
+        response_dict = json.loads(response_exact_rate_place.content)
+        self.assertEqual(response_dict['results'][0]['rate_place'], 5)
+
+        # testing `gt` query param
+
+        response_gt_rate_place = self.client.get('/api/leaderboard/?rate_place__gt=5')
+        response_dict = json.loads(response_gt_rate_place.content)
+        for rec in response_dict['results']:
+            self.assertTrue(rec['rate_place'] > 5)
+
+        response_gte_rate_place = self.client.get('/api/leaderboard/?rate_place__gte=5')
+        response_dict = json.loads(response_gte_rate_place.content)
+        for rec in response_dict['results']:
+            self.assertTrue(rec['rate_place'] >= 5)
+
+        response_exact_user_id = self.client.get('/api/leaderboard/?user_id=150')
+        response_dict = json.loads(response_exact_user_id.content)
+        self.assertEqual(response_dict['results'][0]['user_id'], 150)
+        self.assertEqual(len(response_dict['results']), 1)
